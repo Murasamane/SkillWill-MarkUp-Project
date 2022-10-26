fb5271beacbf61d47bb576efb625a40634bfaf9f
Giorgi Gurgenindze <giorgigurgenidze133@gmail.com>
2022-10-26 14:45:26 +0400

    changed mobile dropdown menu

--- js/main.js
+++ js/main.js
@@ -1,17 +1,29 @@
-const navigation = document.querySelector('.header-section__navigation');
-document.querySelector('.burger-menu--btn').addEventListener('click',()=>{
-    if(navigation.style.display==='flex'){
-        navigation.style.display = "none";
-    }else{
-        navigation.style.display = "flex";
+const navigation = document.querySelector(".header-section__navigation");
+const openIcon = document.querySelector(".btn--open");
+const closeIcon = document.querySelector(".btn--close");
+const BurgerMenu = document
+  .querySelector(".burger-menu--btn")
+  .addEventListener("click", () => {
+    if (navigation.style.display === "flex") {
+      navigation.style.display = "none";
+      closeIcon.style.display = "none";
+      openIcon.style.display = "block";
+    } else {
+      navigation.style.display = "flex";
+      closeIcon.style.display = "block";
+      openIcon.style.display = "none";
     }
-})
+  });
 
-const resetMenu=()=>{
-    if(window.innerWidth>=1060){
-        navigation.style.display="flex";
-    }else if(window.innerWidth<1060){
-        navigation.style.display="none";
-    }
+const resetMenu = () => {
+  if (window.innerWidth >= 1060) {
+    navigation.style.display = "flex";
+    closeIcon.style.display = "none";
+    openIcon.style.display = "none";
+  } else if (window.innerWidth < 1060) {
+    navigation.style.display = "none";
+    openIcon.style.display = "block";
+    closeIcon.style.display = "none";
   }
-  window.addEventListener("resize",resetMenu);
+};
+window.addEventListener("resize", resetMenu);
